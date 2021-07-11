24f34b5417b0502041c443081177f4245edcb6f5
Gina Sala <georgina.sala18@gmail.com>
2021-07-11 15:00:09 -0500

    created temporaryInfo function

--- src/main.js
+++ src/main.js
@@ -1,22 +1,47 @@
-function dropDown(){
-    document.getElementById("favorites").classList.toggle("active");
+function dropDown() {
+  document.getElementById("favorites").classList.toggle("active");
 }
 
 document.getElementById("plus-icon").addEventListener("click", dropDown);
 
-let apiKey = `2a2676887289368652de121a9db03637`;
+let city = "Monterrey";
+
+
 let newDate = new Date();
-let actualDay;
-let actualDays;
-let actualMonth;
-let actualMonths;
-let actualYear;
-let actualDate;
-let actualHour;
-let actualMinutes;
-let multipleConditions = ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squal", "Tornado"];
-let otherConditions = ["Clear", "Clouds", "Snow", "Drizzle", "Rain"];
-let otherConditionsIcons = [`<i class="fas fa-sun"></i>`, `<i class="fas fa-cloud"></i>`, `<i class="fas fa-snowflake"></i>`, `<i class="fas fa-cloud-showers-heavy"></i>`, `<i class="fas fa-cloud-rain"></i>`];
+let roundMainTemp = null;
+let actualDay = null;
+let actualDays = null;
+let actualMonth = null;
+let actualMonths = null;
+let actualYear = null;
+let actualDate = null;
+let actualHour = null;
+let actualMinutes = null;
+
+let multipleConditions = [
+  "Mist",
+  "Smoke",
+  "Haze",
+  "Dust",
+  "Fog",
+  "Sand",
+  "Ash",
+  "Squal",
+  "Tornado"];
+
+let otherConditions = [
+  "Clear",
+  "Clouds",
+  "Snow",
+  "Drizzle",
+  "Rain"];
+
+let otherConditionsIcons = [
+  `<i class="fas fa-sun"></i>`,
+  `<i class="fas fa-cloud"></i>`,
+  `<i class="fas fa-snowflake"></i>`,
+  `<i class="fas fa-cloud-showers-heavy"></i>`,
+  `<i class="fas fa-cloud-rain"></i>`];
 
 function updatePrincipalData(syncDate) {
   actualYear = syncDate.getFullYear();
@@ -24,10 +49,10 @@ function updatePrincipalData(syncDate) {
   actualHour = syncDate.getHours();
   actualMinutes = syncDate.getMinutes();
 
-  if(actualHour < 10){
+  if (actualHour < 10) {
     actualHour = `0${actualHour}`;
   }
-  if(actualMinutes < 10) {
+  if (actualMinutes < 10) {
     actualMinutes = `0${actualMinutes}`;
   }
   let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -44,9 +69,9 @@ let refreshDate = updatePrincipalData(newDate);
 function updateOtherDays() {
   let otherActualDay = newDate.getDay();
   let allDays = document.querySelectorAll(".days");
-  for(let i = 0; i < allDays.length; i++) {
+  for (let i = 0; i < allDays.length; i++) {
     otherActualDay = otherActualDay + 1;
-    if(otherActualDay === 7) {
+    if (otherActualDay === 7) {
       otherActualDay = 0;
     }
     let shortDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -54,38 +79,39 @@ function updateOtherDays() {
   }
 }
 
-function searchForm(Event){
+function searchForm(Event) {
   Event.preventDefault();
   let valueSearch = document.querySelector("#search-city");
-  let cityValue = valueSearch.value;
+  city = valueSearch.value;
   let updateCity = document.querySelector("#city");
-  updateCity.innerHTML = cityValue;
-  apiCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`
-  axios.get(apiCityUrl).then(showWeather);
+  updateCity.innerHTML = city;
+  //apiCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
+  //axios.get(apiCityUrl).then(showWeather);
+  showWeather();
 }
 
 let onlyForm = document.querySelector("#search-form");
 onlyForm.addEventListener("submit", searchForm);
- 
+
 function changeUnits() {
   let actualUnits = document.querySelector("#principal-unit").innerHTML;
   if (actualUnits === "C") {
     let unitsElements = document.querySelectorAll(".units");
-    for( let i = 0; i < unitsElements.length; i++){
+    for (let i = 0; i < unitsElements.length; i++) {
       unitsElements[i].innerHTML = "F";
     }
     let tempElements = document.querySelectorAll(".temperature");
-    for(let i = 0; i < tempElements.length; i++) {
+    for (let i = 0; i < tempElements.length; i++) {
       let operation = Math.round((tempElements[i].innerHTML * 9 / 5) + 32);
       tempElements[i].innerHTML = operation;
     }
-   } else {
+  } else {
     let unitsElements = document.querySelectorAll(".units");
-    for( let i = 0; i < unitsElements.length; i++){
+    for (let i = 0; i < unitsElements.length; i++) {
       unitsElements[i].innerHTML = "C";
     }
     let tempElements = document.querySelectorAll(".temperature");
-    for(let i = 0; i < tempElements.length; i++) {
+    for (let i = 0; i < tempElements.length; i++) {
       let operation = Math.round((tempElements[i].innerHTML - 32) * 5 / 9);
       tempElements[i].innerHTML = operation;
     }
@@ -100,7 +126,7 @@ function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiPositionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiPositionUrl).then(resp =>{
+  axios.get(apiPositionUrl).then(resp => {
     let place = resp.data.name;
     let currentPlace = document.querySelector("#city");
     currentPlace.innerHTML = place;
@@ -109,13 +135,15 @@ function showPosition(position) {
 }
 
 function showWeather(response) {
+  console.log(city);
+  console.log(response);
   let resetUnits = document.querySelectorAll(".units");
-  for( let i = 0; i < resetUnits.length; i++){
+  for (let i = 0; i < resetUnits.length; i++) {
     resetUnits[i].innerHTML = "C";
   }
-  let latitud = response.data.coord.lat;
-  let longitude = response.data.coord.lon;
-  let roundMainTemp = Math.round(response.data.main.temp);
+  //let latitud = response.data.coord.lat;
+  //let longitude = response.data.coord.lon;
+  roundMainTemp = Math.round(response.data.main.temp);
   let description = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
@@ -128,16 +156,16 @@ function showWeather(response) {
   mainTemp.innerHTML = roundMainTemp;
   currentHumidity.innerHTML = humidity;
   currentWind.innerHTML = wind;
-  updateMainIcon(condition, latitud, longitude);
+  //updateMainIcon(condition, latitud, longitude);
 }
 
 function updateMainIcon(response, lat, lon) {
   let mainCondition = response;
-  if(multipleConditions.includes(mainCondition)){
+  if (multipleConditions.includes(mainCondition)) {
     let changeIcon = document.querySelector(".icon-temp");
     changeIcon.innerHTML = `<i class="fas fa-smog"></i>`;
   } else {
-    if(otherConditions.includes(mainCondition)){
+    if (otherConditions.includes(mainCondition)) {
       let position = otherConditions.indexOf(mainCondition);
       let changeIcon = document.querySelector(".icon-temp");
       changeIcon.innerHTML = otherConditionsIcons[position];
@@ -146,36 +174,49 @@ function updateMainIcon(response, lat, lon) {
   getSixDayUrl(lat, lon);
 }
 
-function getSixDayUrl(lat, lon){
+function getSixDayUrl(lat, lon) {
   let sixDaysUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`;
-  axios.get(sixDaysUrl).then(resp =>{
+  axios.get(sixDaysUrl).then(resp => {
     showSixDayTemp(resp);
   });
 }
 
-function showSixDayTemp(response){
+function showSixDayTemp(response) {
   let firstDay = newDate.getDay();
   let allTemps = document.querySelectorAll(".all-temps");
   let changeIcons = document.querySelectorAll(".other-icons");
   let iconPosition;
   let iconsConditions;
-  for(let i = 0; i < allTemps.length; i++) {
-    if(firstDay < 6) {
+  for (let i = 0; i < allTemps.length; i++) {
+    if (firstDay < 6) {
       firstDay = firstDay + 1;
     } else {
       firstDay = 0;
     }
     allTemps[i].innerHTML = Math.round(response.data.daily[firstDay].temp.day);
     iconsConditions = response.data.daily[firstDay].weather[0].main;
-    if(multipleConditions.includes(iconsConditions)){
+    if (multipleConditions.includes(iconsConditions)) {
       changeIcons[i].innerHTML = `<i class="fas fa-smog"></i>`;
     } else {
-      if(otherConditions.includes(iconsConditions)) {
+      if (otherConditions.includes(iconsConditions)) {
         iconPosition = otherConditions.indexOf(iconsConditions);
         changeIcons[i].innerHTML = otherConditionsIcons[iconPosition];
       }
     }
   }
+}
+
+function temporaryInfo(response) {
+  let tempElement = document.querySelector("#main-temp");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let windElement = document.querySelector(".main-wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let descriptionElement = document.querySelector(".condition");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let clickCelsius = document.querySelector("#principal-unit");
@@ -184,3 +225,8 @@ clickCelsius.addEventListener("click", changeUnits);
 
 let clickCurrentLocation = document.querySelector("#location-button");
 clickCurrentLocation.addEventListener("click", showCurrentTemp);
+
+let apiKey = `2a2676887289368652de121a9db03637`;
+let apiCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiCityUrl).then(temporaryInfo);
